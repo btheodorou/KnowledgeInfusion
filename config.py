77e2841f15f1e97f78f52c20f29c646471f403bc
btheodorou <btheodorou@g.hmc.edu>
2023-02-06 16:02:44 -0600

    Setup initial files

---
+++ config.py
@@ -0,0 +1,43 @@
+'''
+    code by Brandon Theodorou
+    Original GPT-2 Paper and repository here: https://github.com/openai/gpt-2
+    Original GPT-2 Pytorch Model: https://github.com/huggingface/pytorch-pretrained-BERT
+    GPT-2 Pytorch Model Derived From: https://github.com/graykode/gpt-2-Pytorch
+'''
+class AutoEHRConfig(object):
+    def __init__(
+            self,
+            total_vocab_size=1624,
+            code_vocab_size=1610,
+            label_vocab_size=12,
+            special_vocab_size=3,
+            n_positions=45,
+            n_ctx=45,
+            n_embd=768,
+            n_layer=12,
+            n_head=12,
+            layer_norm_epsilon=1e-5,
+            initializer_range=0.02,
+            batch_size=256,
+            sample_batch_size=1024,
+            epoch=50,
+            lr=1e-4,
+            rules = [] # list of (which past visits, which codes from past visits, which codes in current visit, which output code in current visit, value to set output code to)
+            
+    ):
+        self.total_vocab_size = total_vocab_size
+        self.code_vocab_size = code_vocab_size
+        self.label_vocab_size = label_vocab_size
+        self.special_vocab_size = special_vocab_size
+        self.n_positions = n_positions
+        self.n_ctx = n_ctx
+        self.n_embd = n_embd
+        self.n_layer = n_layer
+        self.n_head = n_head
+        self.layer_norm_epsilon = layer_norm_epsilon
+        self.initializer_range = initializer_range
+        self.batch_size = batch_size
+        self.sample_batch_size = sample_batch_size
+        self.epoch = epoch
+        self.lr = lr
+        self.rules = rules
