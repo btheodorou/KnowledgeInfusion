fcd4afc40b66ce97b6c18720d88932396e628a99
Brandon Theodorou <bpt3@sunlab-serv-03.cs.illinois.edu>
2023-05-14 11:51:27 -0500

    Add All SPL Stuff minus generation

--- ruleModels/SPL/evaluate_testSet.py
+++ ruleModels/SPL/evaluate_testSet.py
@@ -19,7 +19,7 @@ RUNS = 1
 
 config = HALOConfig()
 NUM_GENERATIONS = 10000
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:5" if torch.cuda.is_available() else "cpu")
 
 # Create circuit object
 cmpe = CircuitMPE('constraints/inpatient.vtree', 'constraints/inpatient.sdd')
@@ -34,80 +34,68 @@ state = torch.load('../../save/spl_model')
 model.load_state_dict(state['model'])
 gate.load_state_dict(state['gate'])
 
-def sample_sequence(model, length, context, batch_size, device='cuda', sample=True):
-  context = torch.tensor(context, device=device, dtype=torch.float32).unsqueeze(0).repeat(batch_size, 1)
-  prev = context.unsqueeze(1)
-  context = None
-  with torch.no_grad():
-    for _ in range(length-1):
-      next = model.sample(prev)
-      theta = gate(next)
-      cmpe.set_params(theta)
-      res = cmpe.get_mpe_inst(next.size(0))
-      next = (res > 0).float()
-      prev = torch.cat((prev, next.unsqueeze(1)), dim=1)
-      
-      if torch.sum(torch.sum(prev[:,:,config.code_vocab_size+config.label_vocab_size+1], dim=1).bool().int(), dim=0).item() == batch_size:
-        break
-  ehr = prev.cpu().detach().numpy()
-  prev = None
-  return ehr
+train_ehr_dataset = pickle.load(open('../../inpatient_data/trainDataset.pkl', 'rb'))
+test_ehr_dataset = pickle.load(open('../../inpatient_data/testDataset.pkl', 'rb'))
+train_c = set([c for p in train_ehr_dataset for v in p['visits'] for c in v])
+test_ehr_dataset = [{'labels': p['labels'], 'visits': [[c for c in v if c in train_c] for v in p['visits'][:96]]} for p in test_ehr_dataset]
 
-def convert_ehr(ehrs, index_to_code=None):
-  ehr_outputs = []
-  for i in range(len(ehrs)):
-    ehr = ehrs[i]
-    ehr_output = []
-    labels_output = ehr[1][config.code_vocab_size:config.code_vocab_size+config.label_vocab_size]
-    if index_to_code is not None:
-      labels_output = [index_to_code[idx + config.code_vocab_size] for idx in np.nonzero(labels_output)[0]]
-    for j in range(2, len(ehr)):
-      visit = ehr[j]
-      visit_output = []
-      indices = np.nonzero(visit)[0]
-      end = False
-      for idx in indices:
-        if idx < config.code_vocab_size: 
-          visit_output.append(index_to_code[idx] if index_to_code is not None else idx)
-        elif idx == config.code_vocab_size+config.label_vocab_size+1:
-          end = True
-      if visit_output != []:
-        ehr_output.append(visit_output)
-      if end:
-        break
-    ehr_outputs.append({'visits': ehr_output, 'labels': labels_output})
-  ehr = None
-  ehr_output = None
-  labels_output = None
-  visit = None
-  visit_output = None
-  indices = None
-  return ehr_outputs
-
-# Generate Synthetic EHR dataset
-speeds = []
-stoken = np.zeros(config.total_vocab_size)
-stoken[config.code_vocab_size+config.label_vocab_size] = 1
-for run in tqdm(range(RUNS)):
-  SEED = run
-  random.seed(SEED)
-  np.random.seed(SEED)
-  torch.manual_seed(SEED)
-  if torch.cuda.is_available():
-    torch.cuda.manual_seed_all(SEED)
+def get_batch(dataset, loc, batch_size):
+    ehr = dataset[loc:loc+batch_size]
+    batch_ehr = np.zeros((len(ehr), config.n_ctx, config.total_vocab_size))
+    batch_lens = np.zeros((len(ehr)), dtype=np.int32)
+    for i, p in enumerate(ehr):
+        visits = p['visits']
+        batch_lens[i] = len(visits) + 1
+        for j, v in enumerate(visits):
+            batch_ehr[i,j+2][v] = 1
+        batch_ehr[i,1,config.code_vocab_size:config.code_vocab_size+config.label_vocab_size] = np.array(p['labels']) # Set the patient labels
+        batch_ehr[i,len(visits)+1,config.code_vocab_size+config.label_vocab_size+1] = 1 # Set the final visit to have the end token
+        batch_ehr[i,len(visits)+2:,config.code_vocab_size+config.label_vocab_size+2] = 1 # Set the rest to the padded visit token
     
-  synthetic_ehr_dataset = []
-  start = time()
-  for i in tqdm(range(0, NUM_GENERATIONS, config.sample_batch_size), leave=False):
-    bs = min([NUM_GENERATIONS-i, config.sample_batch_size])
-    batch_synthetic_ehrs = sample_sequence(model, config.n_ctx, stoken, batch_size=bs, device=device, sample=True)
-    batch_synthetic_ehrs = convert_ehr(batch_synthetic_ehrs)
-    synthetic_ehr_dataset += batch_synthetic_ehrs
-  end = time()
+    batch_ehr[:,0,config.code_vocab_size+config.label_vocab_size] = 1 # Set the first visits to be the start token
+    return batch_ehr, batch_lens, int(batch_lens.sum())
 
-  generationTime = end - start
-  secondsPerPatient = generationTime / NUM_GENERATIONS
-  speeds.append(secondsPerPatient)
-  pickle.dump(secondsPerPatient, open(f'../../results/generationSpeeds/splSpeed_{run}.pkl', 'wb'))
-  pickle.dump(synthetic_ehr_dataset, open(f'../../results/splDataset_{run}.pkl', 'wb'))
-print(f"Seconds Per Patient: {np.mean(speeds)} +/- {np.std(speeds) / np.sqrt(RUNS) * 1.96}")
+model.eval()
+gate.eval()
+log_probability = 0
+n_visits = 0
+n_pos_codes = 0
+n_total_codes = 0
+with torch.no_grad():
+    val_l = []
+    for i in tqdm(range(0, len(test_ehr_dataset), config.batch_size)):
+        batch_ehr, batch_lens, tot_visits = get_batch(test_ehr_dataset, i, config.batch_size)
+        batch_ehr = torch.tensor(batch_ehr, dtype=torch.float32).to(device)
+    
+        output = model(batch_ehr)
+        final_output = torch.zeros(tot_visits, config.n_embd).to(device)
+        final_labels = torch.zeros(tot_visits, config.total_vocab_size).to(device)
+        currStart = 0
+        for j in range(batch_ehr.size(0)):
+            final_output[currStart:currStart+batch_lens[j].item()] = output[j, :batch_lens[j].item()]
+            final_labels[currStart:currStart+batch_lens[j].item()] = batch_ehr[j, 1:batch_lens[j].item()+1]
+            currStart += batch_lens[j].item()
+
+        thetas = gate(final_output)
+        cmpe.set_params(thetas)
+        nll = cmpe.cross_entropy(final_labels, log_space=True).sum()
+        log_probability += -nll.item()
+        n_visits += tot_visits
+        n_pos_codes += final_labels.sum().cpu().item()
+        n_total_codes += final_labels.size(0) * final_labels.size(1)
+
+pp_visit = np.exp(-log_probability/n_visits)
+pp_positive = np.exp(-log_probability/n_pos_codes)
+pp_possible = np.exp(-log_probability/n_total_codes)
+
+metrics_dict = {}
+metrics_dict['Test Log Probability'] = log_probability
+metrics_dict['Perplexity Per Visit'] = pp_visit
+metrics_dict['Perplexity Per Positive Code'] = pp_positive
+metrics_dict['Perplexity Per Possible Code'] = pp_possible
+pickle.dump(metrics_dict, open("../../results/testing_stats/SPL_Metrics.pkl", "wb"))
+
+print('Test Log Probability: ', log_probability)
+print('Perplexity Per Visit: ', pp_visit)
+print('Perplexity Per Positive Code: ', pp_positive)
+print('Perplexity Per Possible Code: ', pp_possible)
